af6c2981945f92bb13de816e799949768a012a88
ewf-updater <ewf-updater@ewf.org>
2018-06-21 11:13:59 +0200

    new style

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,12 +1,13 @@
 function showDropdown() {
-        document.getElementById("#myDropdown").classList.toggle("show");
-    }
+    document.getElementById("#myDropdown").classList.toggle("show");
+    console.log("clickes");
+}
 
-    window.onclick = function(e) {
-        if (!e.target.matches('.dropbtn')) {
-            var myDropdown = document.getElementById("myDropdown");
-            if (myDropdown.classList.contains('show')) {
-                myDropdown.classList.remove('show');
-            }
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
         }
     }
+}
