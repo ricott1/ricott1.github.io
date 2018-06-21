b9db617933fff36a7be3e89d6f6fe802760552be
ewf-updater <ewf-updater@ewf.org>
2018-06-21 11:20:02 +0200

    dropdown menu

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,13 +1,12 @@
 function showDropdown() {
-    document.getElementById("#myDropdown").classList.toggle("show");
-    console.log("clickes");
+    $("#myDropdown").toggleClass("show");
 }
 
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
+        var myDropdown = $("#myDropdown");
+        if (myDropdown.hasClass('show')) {
+            myDropdown.removeClass('show');
         }
     }
 }
